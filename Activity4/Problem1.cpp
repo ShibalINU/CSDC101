b3896d647aa963a77e90a4133e45bb0052aa1d2f
welsh corgi <164317399+ShibalINU@users.noreply.github.com>
2025-10-02 11:30:43 +0800

    Problem1.cpp

--- Activity4/Problem1.cpp
+++ Activity4/Problem1.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+using namespace std;
 
+int main(){
+	
+	float Price = 1;
+	int UserMoney;
+	
+		cout << "How much money do you have? \n";
+		cin >> UserMoney;
+		
+	if (UserMoney >= 1){
+		cout << "Enjoy your lemonade! \n";
+	}else{
+		cout << "Sorry, you need more money \n";
+	}
+	
+	return 0;
+}
