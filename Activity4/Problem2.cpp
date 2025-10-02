88b0045433f2601164c6aaa98c5ce96eb52c8cb9
welsh corgi <164317399+ShibalINU@users.noreply.github.com>
2025-10-02 11:36:56 +0800

    Create Problem2.cpp
    Ufinished

---
+++ Activity4/Problem2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+	float Temperature;
+	float Price;
+	
+	cout << "What is the temperature?";
+	cin >> Temperature;
+	
+	if (Temperature >= 30){
+		Price = 
+	}
+	return 0;
+}
