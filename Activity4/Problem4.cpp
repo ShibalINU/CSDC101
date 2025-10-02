cea4cc0970a544d61061cc15bfb6765ce87cfcbe
welsh corgi <164317399+ShibalINU@users.noreply.github.com>
2025-10-03 00:03:18 +0800

    Problem4.cpp

--- Activity4/Problem4.cpp
+++ Activity4/Problem4.cpp
@@ -20,3 +20,28 @@
 //Sample Output:
 //Total cost: $6.30
 
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+	float Cups, Price = 1, Total, DiscountedPrice;
+		cout << "How many cups? \n"; //user input
+		cin >> Cups;
+
+	if (Cups >= 1 && Cups <= 4){ //range of cup numbers from 1 - 4 will not have discount
+		Total = Cups * Price; //total multiplied to price
+		cout << "Total cost: \n" << Total << "$";
+	}
+	else if (Cups >= 5 && Cups <= 9){// range of cup numbers with 10% discount from 5 - 9
+		Total = Cups * Price * 0.10; // Total cups * price * discount
+		DiscountedPrice = Cups * Price - Total; // discounted price from original price minus the total
+		cout << "Total cost: \n" << DiscountedPrice << "$";
+	}
+	else if (Cups >= 10){ // range of cup numbers with 20% discount 
+		Total = Cups * Price * 0.20;
+		DiscountedPrice = Cups * Price - Total;
+		cout << "Total cost: \n" << DiscountedPrice << "$";
+	}
+	return 0;
+}
