01ac2d4386b4d8699f29dd4e8178a28cb1101a23
welsh corgi <164317399+ShibalINU@users.noreply.github.com>
2025-10-09 14:19:20 +0800

    Update Centerned_pyramid.cpp

--- Loop activity/Centerned_pyramid.cpp
+++ Loop activity/Centerned_pyramid.cpp
@@ -7,7 +7,7 @@ int main()
     int n = 9;
     
 //pyramid
-    for(int i = 1; i < n; i++){
+    for(int i = 1; i <= n; i++){
         for (int j = i; j <= n; j++){
             cout << " "; 
         }
@@ -21,7 +21,7 @@ int main()
     } 
   
   // reverse pyramid  
-       for(int i = 1; i < n; i++){
+       for(int i = 1; i <= n; i++){
         for (int j = 1; j <= i; j++){
             cout << " "; 
         }
