8d5b885d683a76ce455f1f0f51b2a3a42d04c468
welsh corgi <164317399+ShibalINU@users.noreply.github.com>
2025-10-09 14:16:45 +0800

    Create Centerned_pyramid.cpp

---
+++ Loop activity/Centerned_pyramid.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    
+    int n = 9;
+    
+//pyramid
+    for(int i = 1; i < n; i++){
+        for (int j = i; j <= n; j++){
+            cout << " "; 
+        }
+        for (int j = 1; j < i; j++){
+            cout << "*";
+        }
+        for (int j = 1; j <= i; j++){
+            cout << "*";    
+        }
+        cout << endl;
+    } 
+  
+  // reverse pyramid  
+       for(int i = 1; i < n; i++){
+        for (int j = 1; j <= i; j++){
+            cout << " "; 
+        }
+        for (int j = i; j < n; j++){
+            cout << "*";
+        }
+        for (int j = i; j <= n; j++){
+            cout << "*";    
+        }
+        cout << endl;
+    }
+    return 0;
+}
