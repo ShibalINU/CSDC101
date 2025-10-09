1f20c5bf4e05c6f0fddde9834e0984ca38392f95
welsh corgi <164317399+ShibalINU@users.noreply.github.com>
2025-10-09 14:22:54 +0800

    Create hourglass.cpp

---
+++ Loop activity/hourglass.cpp
@@ -0,0 +1,31 @@
+{ //inverted pyramid 1
+    int n = 5;
+
+    for(int i = 1; i <= n; i++){
+        for (int j = 1; j <= i; j++){
+             cout << " "; 
+        }
+            for (int j = i; j < n; j++){
+             cout << "*";
+        }
+            for (int j = i; j <= n; j++){ 
+            cout << "*";    
+        }
+        cout << endl;
+    } //normal pyramid
+  
+    for(int i = n; i >= 1; i--){
+        for (int j = 1; j <= i; j++){
+             cout << " "; 
+        }
+            for (int j = i; j < n; j++){
+             cout << "*";
+        }
+            for (int j = i; j <= n; j++){ 
+            cout << "*";    
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
