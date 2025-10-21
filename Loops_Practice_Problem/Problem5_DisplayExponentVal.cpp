403e7b8721f156013f010343e97f607bc09072c9
welsh corgi <164317399+ShibalINU@users.noreply.github.com>
2025-10-21 11:07:48 +0800

    Add program to display exponentiation result

---
+++ Loops_Practice_Problem/Problem5_DisplayExponentVal.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int base, exponent, i, result = 1;
+    cout << "Enter base: ";
+    cin >> base;
+    cout << "Enter exponent: ";
+    cin >> exponent;
+    for(i = 1; i <= exponent; i++){
+        result = result * base;
+    }
+    cout << base << " ^ " << exponent << " = " << result << endl;
+   
+return 0;
+} 
