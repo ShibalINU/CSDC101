a9a9ed8f6cbf565c854321a142a7b0f11ae8a8bc
welsh corgi <164317399+ShibalINU@users.noreply.github.com>
2025-10-21 11:03:30 +0800

    Implement sum of natural numbers using while loop
    Added a while loop implementation for summing natural numbers.

--- Loops_Practice_Problem/Problem_3_SumOfNaturalNum.cpp
+++ Loops_Practice_Problem/Problem_3_SumOfNaturalNum.cpp
@@ -1,13 +1,31 @@
+//for loop
 #include <iostream>
 using namespace std;
 
-int main(){
+int main(){ 
     int n, i, sum = 0;
     cout << "Enter an integer: ";
     cin >> n;
     
     for(i = 1; i <= n; i++){
         sum = sum + i;
+    }
+    cout << "The sum is: " << sum << endl;
+return 0;
+}
+
+//While loop
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n, i = 1, sum = 0;
+    cout << "Enter an integer: ";
+    cin >> n;
+    
+    while(i <= n){
+        sum = sum + i;
+        i++;
     }
     cout << "The sum is: " << sum << endl;
 return 0;
