479dcf8bf672ea87a712205c066f7e6cb16a8df8
welsh corgi <164317399+ShibalINU@users.noreply.github.com>
2025-10-23 07:22:37 +0800

    Update Problem5_DisplayExponentVal.cpp

--- Loops_Practice_Problem/Problem5_DisplayExponentVal.cpp
+++ Loops_Practice_Problem/Problem5_DisplayExponentVal.cpp
@@ -2,15 +2,17 @@
 using namespace std;
 
 int main(){
-    int base, exponent, i, result = 1;
+    int base, nextbase, exponent, i, result = 1;
     cout << "Enter base: ";
     cin >> base;
     cout << "Enter exponent: ";
     cin >> exponent;
     for(i = 1; i <= exponent; i++){
         result = result * base;
+        nextbase = result;
+        cout << nextbase << " x " << base << " = " << result << endl;
     }
     cout << base << " ^ " << exponent << " = " << result << endl;
    
 return 0;
-} 
+}
